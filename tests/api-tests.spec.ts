bcd885efa42a2ea41b41c9da10722a8cf44ac39f
frederic-hallein <frederic.hallein@outlook.com>
2024-12-06 11:14:17 +0100

    changed .json file and test

--- tests/api-tests.spec.ts
+++ tests/api-tests.spec.ts
@@ -1,27 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-import TestDataPets from "../test-data/pets.json";
+import { TestDataPets } from "../test-data/pets.json";
 
 test.describe("API Pet Tests - all API tests related to the 'Pets' section of Petstore", () => {
-  for (const [petType, petTypeList] of Object.entries(TestDataPets)) {
-    for (const [_, pet] of Object.entries(petTypeList)) {
-      test(`POST ${petType} - name: ${pet.name}`, async ({ request }) => {
-        const postPetResponse = await request.post("pet", {
-          data: {
-            "id": pet.id,
-            "category": {
-              "id"  : pet.category.id,
-              "name": pet.category.name
-            },
-            "name"     : pet.name,
-            "photoUrls": pet.photoUrls,
-            "tags"     : pet.tags,
-            "status"   : pet.status
-          }
-        });
-        const postPetResponseJSON = await postPetResponse.json(); 
-        await expect(postPetResponse).toBeOK();
+  for (const [_, pet] of Object.entries(TestDataPets)) {
+    test(`POST pet with name ${pet.name}`, async ({ request }) => {
+      const postPetResponse = await request.post("pet", {
+        data: {
+          "id": pet.id,
+          "category": {
+            "id"  : pet.category.id,
+            "name": pet.category.name
+          },
+          "name"     : pet.name,
+          "photoUrls": pet.photoUrls,
+          "tags"     : pet.tags,
+          "status"   : pet.status
+        }
       });
-    }
+      const postPetResponseJSON = await postPetResponse.json(); 
+      await expect(postPetResponse).toBeOK();
+    });
+    
   }
 });
